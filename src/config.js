a9135bbb923fac662d385bb11ef7a2713e85cf3b
unknown <mmokbel@voyancemed.com>
2022-09-24 21:02:02 +0200

    handle config file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    backendDev : {
+        baseUrl: 'https://jsonplaceholder.typicode.com', 
+        headers: {
+        "Content-type" : 'application/json', 
+        }
+       
+   
+    }, 
+    keySecret: 'make-key-secure-for-clients', 
+    valueSecret : 'make-app-secure-for-clients', 
+    encodingType : 'aes'
+};
